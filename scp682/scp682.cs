09e10f3bda4a66117ec852de2c54853952c1fba6
cushaw1 <43043675+cushaw1@users.noreply.github.com>
2018-10-10 20:27:41 +0800

    Update scp682.cs

--- scp682/scp682.cs
+++ scp682/scp682.cs
@@ -1,4 +1,4 @@
-﻿using Smod2;
+using Smod2;
 using Smod2.Attributes;
 using scp_682;
 
@@ -9,7 +9,7 @@ namespace scp682
     name = "scp682",
     description = "939-89 maybe become 682",
     id = "cushaw.scp682",
-    version = "1.0",
+    version = "1.1",
     SmodMajor = 3,
     SmodMinor = 1,
     SmodRevision = 18
@@ -29,14 +29,14 @@ namespace scp682
         public override void Register()
         {
             this.AddEventHandlers(new EventHandler(this));
-            this.AddConfig(new Smod2.Config.ConfigSetting("scp682_enable", true, Smod2.Config.SettingType.BOOL, true, ""));
-            this.AddConfig(new Smod2.Config.ConfigSetting("scp682_door", true, Smod2.Config.SettingType.BOOL, true, ""));
-            this.AddConfig(new Smod2.Config.ConfigSetting("scp682_hp", new int { }, Smod2.Config.SettingType.NUMERIC, true, ""));
-            this.AddConfig(new Smod2.Config.ConfigSetting("scp682_attack", new int { }, Smod2.Config.SettingType.NUMERIC, true, ""));
-            this.AddConfig(new Smod2.Config.ConfigSetting("scp682_spawn", new int { }, Smod2.Config.SettingType.NUMERIC, true, ""));
-            this.AddConfig(new Smod2.Config.ConfigSetting("scp682_heal", new int { }, Smod2.Config.SettingType.NUMERIC, true, ""));
-            this.AddConfig(new Smod2.Config.ConfigSetting("scp682_healtime", new int { }, Smod2.Config.SettingType.NUMERIC, true, ""));
-            this.AddConfig(new Smod2.Config.ConfigSetting("scp682_eat", new int { }, Smod2.Config.SettingType.NUMERIC, true, ""));
+            this.AddConfig(new Smod2.Config.ConfigSetting("scp682_enable", true, Smod2.Config.SettingType.BOOL, true, "682 plugin enable/disable"));
+            this.AddConfig(new Smod2.Config.ConfigSetting("scp682_door", true, Smod2.Config.SettingType.BOOL, true, "682 destroy door enable/disable"));
+            this.AddConfig(new Smod2.Config.ConfigSetting("scp682_kill", true, Smod2.Config.SettingType.BOOL, true, "682 kill human by one attack enable/disable"));
+            this.AddConfig(new Smod2.Config.ConfigSetting("scp682_spawn", 30, Smod2.Config.SettingType.NUMERIC, true, "	682 spawn probability(%)"));
+            this.AddConfig(new Smod2.Config.ConfigSetting("scp682_heal_hp",5, Smod2.Config.SettingType.NUMERIC, true, "682 automatic heal hp"));
+            this.AddConfig(new Smod2.Config.ConfigSetting("scp682_heal_time", 1, Smod2.Config.SettingType.NUMERIC, true, "682 automatic heal time(s)"));
+            this.AddConfig(new Smod2.Config.ConfigSetting("scp682_heal_maxhp", 2200, Smod2.Config.SettingType.NUMERIC, true, "682 heal max HP"));
+            this.AddConfig(new Smod2.Config.ConfigSetting("scp682_eat_hp", 100, Smod2.Config.SettingType.NUMERIC, true, "682 heal HP with eat human"));
         }
     }
 }
